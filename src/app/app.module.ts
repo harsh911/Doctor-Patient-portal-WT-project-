f4155defcb58857aebe6988761a044a0d3e6be43
harsh911 <harshped37@gmail.com>
2019-08-31 19:17:42 +0530

    basic layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppointmentComponent } from './appointments/appointment/appointment.component';
+import { DetailsComponent } from './medical/details/details.component';
+import { DoctorListComponent } from './doctors/doctor-list/doctor-list.component';
+import { RegisterComponent } from './register/register/register.component';
+import { LoginComponent } from './register/login/login.component';
+import { HeaderComponent } from './header/header/header.component';
+import { FooterComponent } from './footer/footer/footer.component';
+import { TabsComponent } from './tabs/tabs/tabs.component';
+import { ContentComponent } from './content/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppointmentComponent,
+    DetailsComponent,
+    DoctorListComponent,
+    RegisterComponent,
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent,
+    TabsComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule
